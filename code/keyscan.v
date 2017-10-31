c81a1a05da7bb9470b36a354d26fbbff3bff82d6
PanupongDeve <5635512110@psu.ac.th>
2017-10-31 07:44:58 +0700

    update code

--- code/keyscan.v
+++ code/keyscan.v
@@ -15,47 +15,61 @@ assign common = 0;
 
 reg [1:0] state = st1;
 
-always@(posedge clk)
-    case (state)
-        st1: begin
-            row <= 4'b0001;
-            case (column)
-                3'b001 : display <= 7'b0110000;
-                3'b010 : display <= 7'b1101101;
-                3'b100 : display <= 7'b1111001;
-            endcase
-            state <= st2;
-        end
-        
-        st2: begin
-            row <= 4'b0010;
-            case (column)
-                3'b001 : display <= 7'b0110011;
-                3'b010 : display <= 7'b1011011;
-                3'b100 : display <= 7'b1111001;
-            endcase
-            state <= st3
-        end
+reg sclk;
+reg [31:0] cnt;
 
-        st3: begin
-            row <= 4'b0100;
-            case (column)
-                3'b001 : display <= 7'b1110000;
-                3'b010 : display <= 7'b1111111;
-                3'b100 : display <= 7'b0011111;
-            endcase
-            state <= st4;
-        end
+    always@(posedge sclk)
+        case (state)
+            st1: begin
+                row <= 4'b0001;
+                case (column)
+                    3'b001 : display <= 7'b0110000;
+                    3'b010 : display <= 7'b1101101;
+                    3'b100 : display <= 7'b1111001;
+                endcase
+                state <= st2;
+            end
+            
+            st2: begin
+                row <= 4'b0010;
+                case (column)
+                    3'b001 : display <= 7'b0110011;
+                    3'b010 : display <= 7'b1011011;
+                    3'b100 : display <= 7'b1111001;
+                endcase
+                state <= st3
+            end
 
-        st4: begin
-            row <= 4'b1000;
-            case (column)
-                3'b001 : display <= 7'b1110111;
-                3'b010 : display <= 7'b1111110;
-                3'b100 : display <= 7'b0011111;
-            endcase
-            state <= st1;
+            st3: begin
+                row <= 4'b0100;
+                case (column)
+                    3'b001 : display <= 7'b1110000;
+                    3'b010 : display <= 7'b1111111;
+                    3'b100 : display <= 7'b0011111;
+                endcase
+                state <= st4;
+            end
+
+            st4: begin
+                row <= 4'b1000;
+                case (column)
+                    3'b001 : display <= 7'b1110111;
+                    3'b010 : display <= 7'b1111110;
+                    3'b100 : display <= 7'b0011111;
+                endcase
+                state <= st1;
+            end
+            default : display <= 7'b1000111;
+        endcase
+
+    always@(posedge clk)
+        begin
+            if(cnt == 25000000)
+                begin
+                    sclk <= ~sclk;
+                    snt <= 0;
+                end
+            else
+                cnt <= cnt + 1;
         end
-        default : display <= 7'b1000111;
-    endcase
 endmodule
